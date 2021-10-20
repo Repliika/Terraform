70db2c9d123b4289fa13a73d221c745074b2a689
Repliika <liddergg@gmail.com>
2021-10-20 12:18:14 -0400

    created docker image resource

--- main.tf
+++ main.tf
@@ -2,9 +2,15 @@ terraform {
   required_providers {
     docker = {
       source  = "kreuzwerker/docker"
-      version = "2.15.0"
+      version = "~>2.15.0"
     }
   }
 }
 
-provider "docker" {}
+provider "docker" {
+  host = "npipe:////.//pipe//docker_engine"
+}
+
+resource "docker_image" "ncdered_image" {
+  name = "nodered/node-red:latest"
+}
