8c3e048172ca00810c661a341d9bb76e2e71963c
Repliika <liddergg@gmail.com>
2021-10-21 10:22:22 -0400

    Adding output to print IP address so we don't have to go into state file

--- main.tf
+++ main.tf
@@ -11,6 +11,20 @@ provider "docker" {
   host = "npipe:////.//pipe//docker_engine"
 }
 
-resource "docker_image" "ncdered_image" {
+resource "docker_image" "nodered_image" {
   name = "nodered/node-red:latest"
+}
+
+resource "docker_container" "nodered_container" {
+  name  = "nodered"
+  image = docker_image.nodered_image.latest
+  ports {
+    internal = 1800
+    external = 1800 #defaults and match dockerfile. 
+  }
+}
+
+output "IP_address" {
+  value       = docker_container.nodered_container.ip_address
+  description = "prints IP address of nodered container"
 }
